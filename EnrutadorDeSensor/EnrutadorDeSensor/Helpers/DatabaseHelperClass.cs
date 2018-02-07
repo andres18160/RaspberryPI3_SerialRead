586231ab01b2b9f9b441f333c7faa4274e034557
Camilo Andres Agudelo <andres18160@gmail.com>
2018-02-07 16:43:12 -0500

    Primera publicacion

---
+++ EnrutadorDeSensor/EnrutadorDeSensor/Helpers/DatabaseHelperClass.cs
@@ -0,0 +1,123 @@
+﻿using EnrutadorDeSensor.Models;
+using SQLite.Net;
+using SQLite.Net.Platform.WinRT;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnrutadorDeSensor.Helpers
+{
+    class DatabaseHelperClass
+    {
+        //Create Tabble   
+        public void CreateDatabase(string DB_PATH)
+        {
+            if (!CheckFileExists(DB_PATH).Result)
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(new SQLitePlatformWinRT(), DB_PATH))
+                {
+                    conn.CreateTable<Lectura>();
+
+                }
+            }
+        }
+        private async Task<bool> CheckFileExists(string fileName)
+        {
+            try
+            {
+                var store = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync(fileName);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        // Insert the new contact in the Contacts table.   
+        public void Insert(Lectura _lectura)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(new SQLitePlatformWinRT(), App.DB_PATH))
+            {
+                conn.RunInTransaction(() =>
+                {
+                    conn.Insert(_lectura);
+                });
+            }
+        }
+        // Retrieve the specific contact from the database.     
+        public Lectura ReadContact(int id)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(new SQLitePlatformWinRT(), App.DB_PATH))
+            {
+                var existLectura = conn.Query<Lectura>("select * from Lectura where Id =" + id).FirstOrDefault();
+                return existLectura;
+            }
+        }
+        public ObservableCollection<Lectura> ReadAllLecturas()
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(new SQLitePlatformWinRT(), App.DB_PATH))
+                {
+                    List<Lectura> myCollection = conn.Table<Lectura>().ToList<Lectura>();
+                    ObservableCollection<Lectura> LecturaList = new ObservableCollection<Lectura>(myCollection);
+                    return LecturaList;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+        //Update existing Lectura   
+        public void UpdateDetails(Lectura _Lectura)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(new SQLitePlatformWinRT(), App.DB_PATH))
+            {
+
+                var existLectura = conn.Query<Lectura>("select * from Lectura where Id =" + _Lectura.Id).FirstOrDefault();
+                if (existLectura != null)
+                {
+
+                    conn.RunInTransaction(() =>
+                    {
+                        conn.Update(_Lectura);
+                    });
+                }
+
+            }
+        }
+        //Delete all LecturaList or delete Lectura table     
+        public void DeleteLectura()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(new SQLitePlatformWinRT(), App.DB_PATH))
+            {
+
+                conn.DropTable<Lectura>();
+                conn.CreateTable<Lectura>();
+                conn.Dispose();
+                conn.Close();
+
+            }
+        }
+        //Delete specific Lectura     
+        public void DeleteLectura(int Id)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(new SQLitePlatformWinRT(), App.DB_PATH))
+            {
+
+                var existLectura = conn.Query<Lectura>("select * from Lectura where Id =" + Id).FirstOrDefault();
+                if (existLectura != null)
+                {
+                    conn.RunInTransaction(() =>
+                    {
+                        conn.Delete(existLectura);
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ EnrutadorDeSensor/EnrutadorDeSensor/Models/Lectura.cs
@@ -0,0 +1,43 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnrutadorDeSensor.Models
+{
+  public  class Lectura
+    {
+
+        [PrimaryKey, AutoIncrement]  
+        public int Id { get; set; }
+        public string Presion { get; set; }
+
+        public string Temperatura { get; set; }
+        public string Humedad { get; set; }
+        public string Fecha { get; set; }
+
+        public bool Estado { get; set; }
+
+
+        public Lectura()
+        {
+
+        }
+
+        public Lectura(string _Presion,string _Temperatura,string _Humedad)
+        {
+            Presion = _Presion;
+            Temperatura = _Temperatura;
+            Humedad = _Humedad;
+            Fecha = DateTime.Now.ToString();
+            Estado = false;
+        }
+
+
+
+    }
+
+
+}
